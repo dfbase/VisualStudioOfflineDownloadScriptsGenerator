6181ab046bed0818174b8247d728a5fd56bdfd19
dfbase <dfbase@shocktunnel.com>
2023-01-27 16:29:42 +0800

    Add Export scripts.

--- VisualStudioOfflineDownloadScriptsGenerator/ScriptsWindow.xaml.cs
+++ VisualStudioOfflineDownloadScriptsGenerator/ScriptsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Ookii.Dialogs.Wpf;
 
 namespace VisualStudioOfflineDownloadScriptsGenerator
 {
@@ -19,6 +20,25 @@ namespace VisualStudioOfflineDownloadScriptsGenerator
         public ScriptsWindow()
         {
             InitializeComponent();
+
+            btnExport.Click += BtnExport_Click;
+        }
+
+        private void BtnExport_Click(Object sender, RoutedEventArgs e)
+        {
+            var ofd = new VistaFolderBrowserDialog()
+            {
+                RootFolder = Environment.SpecialFolder.DesktopDirectory,
+                ShowNewFolderButton = true
+            };
+
+            if (ofd.ShowDialog() == true && !String.IsNullOrWhiteSpace(ofd.SelectedPath))
+            {
+                File.WriteAllText(Path.Combine(ofd.SelectedPath, "下载脚本.txt"), txtDownloadScript.Text, Encoding.UTF8);
+                File.WriteAllText(Path.Combine(ofd.SelectedPath, "校验脚本.txt"), txtVerifyScript.Text, Encoding.UTF8);
+
+                MessageBox.Show("导出成功！", "系统提示", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
 
         public void SetDownloadScript(String script)
